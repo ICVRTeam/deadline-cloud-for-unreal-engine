3f2c2be04f82a7d4856118c4a2247ffaa0d9a219
Ilona Alekseeva <i.alekseeva@icvr.io>
2024-10-08 15:06:16 +0300

    add ConsistencyChecker lib

---
+++ src/unreal_plugin/Source/UnrealDeadlineCloudService/Public/PythonAPILibraries/PythonParametersConsistencyChecker.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PythonAPILibrary.h"
+#include "PythonYamlLibrary.h"
+#include "UObject/Object.h"
+#include "DeadlineCloudJobSettings/DeadlineCloudJob.h"
+#include "DeadlineCloudJobSettings/DeadlineCloudStep.h"
+#include "DeadlineCloudJobSettings/DeadlineCloudEnvironment.h"
+#include "PythonParametersConsistencyChecker.generated.h"
+
+USTRUCT(BlueprintType)
+struct FParametersConsistencyCheckResult 
+
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Checker")
+	bool Passed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Checker")
+	FString Reason;
+
+};
+
+
+
+UCLASS()
+class UNREALDEADLINECLOUDSERVICE_API UPythonParametersConsistencyChecker : public UObject, public TPythonAPILibraryBase<UPythonParametersConsistencyChecker>
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintImplementableEvent)
+	FParametersConsistencyCheckResult	CheckJobParametersConsistency(UDeadlineCloudJob* Job);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void FixJobParametersConsistency (UDeadlineCloudJob* Job);
+   
+	UFUNCTION(BlueprintImplementableEvent)
+	FParametersConsistencyCheckResult	CheckStepParametersConsistency(UDeadlineCloudStep* Step);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void	FixStepParametersConsistency (UDeadlineCloudStep* Step);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	FParametersConsistencyCheckResult	CheckEnvironmentParametersConsistency(UDeadlineCloudEnvironment* Environment);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void	FixEnvironmentParametersConsistency (UDeadlineCloudEnvironment* Environment);
+ 
+};
